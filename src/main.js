ba2fe0e74080c9c2a29b6b7cc24a4f75cc10951a
zemlyansky <anton.zemlyansky@gmail.com>
2020-05-09 09:53:07 +0300

    Add new datasets, fix scientific notations

--- src/main.js
+++ src/main.js
@@ -13,9 +13,16 @@ module.exports = function gen (p) {
   const colNames = X[0].map((_, i) => 'x' + (i + 1)).concat('y')
   const data = X.map((x, i) => x.concat(y[i]))
 
-  let res
+  let res = ''
   if (params.format === 'csv') {
-    res = colNames.join(',') + '\n' + data.map(row => row.join(',')).join('\n') + '\n'
+    res = colNames.join(',') + '\n'
+    // To prevent scientific notation, toFixed, then trim right zeros and '.'
+    // Tried this: https://stackoverflow.com/questions/26299160/using-regex-how-do-i-remove-the-trailing-zeros-from-a-decimal-number#26299205
+    // Doesn't work with negative numbers. So more transparent script:
+    res += data.map(row => row.map(v => {
+      const vstr = v.toString()
+      return vstr.includes('e') ? v.toFixed(15).replace(/0*$/, '') : vstr
+    }).join(',')).join('\n') + '\n'
   } else {
     res = JSON.stringify({
       data: data.map(row => {
